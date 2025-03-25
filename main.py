17aea0f9c8d9cbf194a47cb92ee95dca7b80fe37
Lopez_HosheaShania <hosheashaniaclopez@iskolarngbayan.pup.edu.ph>
2025-03-26 03:38:05 +0800

    add main file with to do tasks

---
+++ main.py
@@ -0,0 +1,38 @@
+def add_products():
+    # TODO (Adriel Dimayuga):
+    # Add a function that allows the user to input the following:
+    # - Product Name
+    # - Price
+    # - Quantity
+    pass
+
+def user_choice():
+    # TODO (Grace Lim): 
+    # loop the function "add_products", ask the user if it wants to add 
+    # another product using y (yes) or n (no)
+    pass
+
+def senior_discount():
+    # TODO (Althea Aragon):
+    # Add a function that deducts 10% of the grand total of the products
+    # if the user has typed in a Senior ID no.
+    pass
+
+def print_receipt():
+    # TODO (Hoshea Lopez, Grace Lim):
+    # Add a function that displays:
+    # - Items (product name, price, quantity, total)
+    # - Customer Name
+    # - Senior ID no.
+    # - Grand Total
+    pass
+
+# TODO (Rain Romero):
+# Add a code that will allow the user to input Customer Name and Senior ID (Optional).
+# Use "senior_deduction" function to compute for the grand total
+
+# TODO (Grace Lim):
+# Compute the grand total outside of the function for direct computation
+
+# TODO (Rain Romero):
+# call the "print_receipt" function as final output
